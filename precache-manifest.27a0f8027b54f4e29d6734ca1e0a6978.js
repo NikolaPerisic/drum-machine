2b9fb356592906a094403bda3b47b7904289bbfc
Nikola Perisic <cathode.y2k@gmail.com>
2019-02-26 00:24:33 +0100

    Updates

---
+++ precache-manifest.27a0f8027b54f4e29d6734ca1e0a6978.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "05e519ee08f290689aa108a8967296b2",
+    "url": "/drum-machine/static/media/tr808_kick_01.05e519ee.wav"
+  },
+  {
+    "revision": "17992acc0da260f67ee2",
+    "url": "/drum-machine/static/css/main.e852efc7.chunk.css"
+  },
+  {
+    "revision": "c73e5ac2cc3e61c10a29",
+    "url": "/drum-machine/static/js/runtime~main.c73e5ac2.js"
+  },
+  {
+    "revision": "4c63ce56e856b6c393b2",
+    "url": "/drum-machine/static/js/2.4c63ce56.chunk.js"
+  },
+  {
+    "revision": "dd3d636abc6b9f799b5133a61b93d689",
+    "url": "/drum-machine/static/media/tr808_clap.dd3d636a.wav"
+  },
+  {
+    "revision": "8c16180e49cfb163065e648b98c561db",
+    "url": "/drum-machine/static/media/tr808_clave.8c16180e.wav"
+  },
+  {
+    "revision": "1a694aff0dc62aa0e26dd0f53a3e26aa",
+    "url": "/drum-machine/static/media/tr808_closedhat_01.1a694aff.wav"
+  },
+  {
+    "revision": "17992acc0da260f67ee2",
+    "url": "/drum-machine/static/js/main.17992acc.chunk.js"
+  },
+  {
+    "revision": "bf9a5194b4f48d0bfc2be675ca564dad",
+    "url": "/drum-machine/static/media/tr808_kick_03.bf9a5194.wav"
+  },
+  {
+    "revision": "7bccfb5ff8784341d1351313a5640f77",
+    "url": "/drum-machine/static/media/tr808_kick_11.7bccfb5f.wav"
+  },
+  {
+    "revision": "9a20640ae566401be0a46294e4b8ac66",
+    "url": "/drum-machine/static/media/tr808_rimshot.9a20640a.wav"
+  },
+  {
+    "revision": "51fed6677c0134c7aa5cbe8eb53b8360",
+    "url": "/drum-machine/static/media/tr808_snare_14.51fed667.wav"
+  },
+  {
+    "revision": "e3ca9799bf43c8f56445291abee2a1b9",
+    "url": "/drum-machine/static/media/tr808_tom_08.e3ca9799.wav"
+  },
+  {
+    "revision": "783c45de649720f5e7307b45274a9ac7",
+    "url": "/drum-machine/static/media/bckgrnd3.783c45de.jpeg"
+  },
+  {
+    "revision": "90f68d798e70c877565af24e2e93e0bc",
+    "url": "/drum-machine/index.html"
+  }
+];
