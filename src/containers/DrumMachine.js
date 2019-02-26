4927b9c9eaec12a36386182698ebed5b1c9bdb0e
Nikola Perisic <cathode.y2k@gmail.com>
2019-02-26 16:14:13 +0100

    refactored state for better performance

--- src/containers/DrumMachine.js
+++ src/containers/DrumMachine.js
@@ -1,64 +1,136 @@
 import React, { Component } from "react";
 import Soundpad from "../components/Soundpad/Soundpad";
 import "./DrumMachine.scss";
-import data from "../assets/Data/Data";
+import sounds from "../assets/sounds/sounds";
 
 class DrumMachine extends Component {
     state = {
-        data: [...data],
-        display: "TR808 beats"
+        clap: {
+            id: "clap",
+            hotkey: "Q",
+            source: sounds.clap,
+            keyCode: 81,
+            active: false
+        },
+        clave: {
+            id: "clave",
+            hotkey: "W",
+            source: sounds.clave,
+            keyCode: 87,
+            active: false
+        },
+        closethat: {
+            id: "closedhat",
+            hotkey: "E",
+            source: sounds.closedHat,
+            keyCode: 69,
+            active: false
+        },
+        kick_01: {
+            id: "kick_01",
+            hotkey: "A",
+            source: sounds.kick01,
+            keyCode: 65,
+            active: false
+        },
+        kick_03: {
+            id: "kick_03",
+            hotkey: "S",
+            source: sounds.kick03,
+            keyCode: 83,
+            active: false
+        },
+        kick_11: {
+            id: "kick_11",
+            hotkey: "D",
+            source: sounds.kick11,
+            keyCode: 68,
+            active: false
+        },
+        rimshot: {
+            id: "rimshot",
+            hotkey: "Z",
+            source: sounds.rimshot,
+            keyCode: 90,
+            active: false
+        },
+        snare: {
+            id: "snare",
+            hotkey: "X",
+            source: sounds.snare,
+            keyCode: 88,
+            active: false
+        },
+        tom: {
+            id: "tom",
+            hotkey: "C",
+            source: sounds.tom,
+            keyCode: 67,
+            active: false
+        },
+        display: "TR808 Beats"
     };
+
     componentDidMount() {
         document.addEventListener("keydown", this.keyPressedHandler);
     }
+
     componentWillUnmount() {
         document.removeEventListener("keydown", this.keyPressedHandler);
     }
-    playSoundHandler = (el, i) => {
+
+    playSoundHandler = (el, key) => {
         const sound = new Audio(el.source);
-        this.displayInstrumentHandler(el.id);
-        this.bounceAnimationHandler(i);
+        this.bounceAnimationHandler(key);
+        this.updateDisplayHandler(key);
         setTimeout(() => {
-            this.bounceAnimationHandler(i);
+            this.bounceAnimationHandler(key);
         }, 100);
         return sound.play();
     };
+
     keyPressedHandler = el => {
-        this.state.data.map(item => {
-            if (el.keyCode === item.keyCode) {
-                let index = this.state.data.indexOf(item);
-                this.playSoundHandler(item, index);
-                this.displayInstrumentHandler(item.id);
+        Object.entries(this.state).map(([key, value]) => {
+            if (el.keyCode === value.keyCode) {
+                return this.playSoundHandler(value, key);
+            } else {
+                return null;
             }
-            return null;
         });
     };
-    displayInstrumentHandler = el => {
-        this.setState({ display: el });
+
+    bounceAnimationHandler = key => {
+        let data = { ...this.state[key] };
+        data.active ? (data.active = false) : (data.active = true);
+        this.setState({ [key]: data });
     };
-    bounceAnimationHandler = i => {
-        let data = [...this.state.data];
-        data[i].active ? (data[i].active = false) : (data[i].active = true);
-        this.setState({ data: data });
+
+    updateDisplayHandler = key => {
+        let display = key.replace("_", " ");
+        this.setState({ display: display });
     };
 
     render() {
-        const soundpads = this.state.data.map((el, index) => {
-            return (
-                <Soundpad
-                    key={index}
-                    id={el.id}
-                    hotkey={el.hotkey}
-                    bounce={el.active}
-                    playSound={() => this.playSoundHandler(el, index)}
-                    keyPress={this.keyPressed}
-                />
-            );
+        const soundpads = Object.entries(this.state).map(([key, value]) => {
+            if (value !== this.state.display) {
+                return (
+                    <Soundpad
+                        key={key}
+                        id={value.id}
+                        hotkey={value.hotkey}
+                        bounce={value.active}
+                        playSound={() => this.playSoundHandler(value, key)}
+                        keyPress={this.keyPressed}
+                    />
+                );
+            } else {
+                return null;
+            }
         });
         return (
             <div id="drum-machine">
                 <div id="display">
-                    <p>{this.state.display.replace("_", " ")}</p>
+                    <p>{this.state.display}</p>
                 </div>
                 <div className="Wrapper">{soundpads}</div>
             </div>
